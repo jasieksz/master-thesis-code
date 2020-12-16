7a34ee5b918430511d2b9a902ce2bb5f18e13b71
Jan Sznajd <jasiek.sznajd@gmail.com>
2020-12-16 16:20:51 +0100

    Started prepearing notebook with Spark to run on EMR

--- code/profiles/consecutiveOnesDetection.py
+++ code/profiles/consecutiveOnesDetection.py
@@ -1,5 +1,5 @@
 from numpy import ndarray
-
+from code.profiles.generation import getProfilePermutations
 
 def consecutiveOnes1D(A: ndarray) -> bool:
     ones: bool = False
@@ -13,14 +13,16 @@ def consecutiveOnes1D(A: ndarray) -> bool:
             return False
     return True
 
-
-def consecutiveOnes2D(approvalSet: ndarray) -> bool:
-    return consecutiveOnesCR(approvalSet) or consecutiveOnesVR(approvalSet)
-
-
 def consecutiveOnesCR(approvalSet: ndarray) -> bool:
     return all([consecutiveOnes1D(approvalSet[:, i]) for i in range(approvalSet.shape[1])])
 
 
 def consecutiveOnesVR(approvalSet: ndarray) -> bool:
     return all([consecutiveOnes1D(approvalSet[i, :]) for i in range(approvalSet.shape[0])])
+
+
+def consecutiveOnes2D(approvalSet: ndarray) -> bool:
+    return consecutiveOnesCR(approvalSet) or consecutiveOnesVR(approvalSet)
+
+def totalconsecutiveOnes2D(approvalSet: ndarray) -> bool:
+    return any([consecutiveOnes2D(approvalSet=a) for a in getProfilePermutations(approvalSet)])
