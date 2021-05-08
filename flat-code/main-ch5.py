72187fc4c21dcc6df965cf655c4205797c39b5d2
Jan Sznajd <jasiek.sznajd@gmail.com>
2021-05-08 23:08:56 +0200

    almost ready to generate data for the experiments

--- flat-code/main-ch5.py
+++ flat-code/main-ch5.py
@@ -150,22 +150,20 @@ sns.displot(data=df2, x='x')
 
 #%%
 
-def profileFromVotersAndCandidates(voters: np.ndarray, candidates: np.ndarray) -> Profile:
+def npProfileFromVotersAndCandidates(voters: np.ndarray, candidates: np.ndarray) -> np.ndarray:
     A = np.zeros((len(voters),len(candidates)))
     for vI, (vX,vR) in enumerate(voters):
         for cI, (cX,cR) in enumerate(candidates):
             if vcrPropertyRaw(vX, vR, cX, cR):
                 A[vI,cI] = 1
     
-    npProfile = np.concatenate([
+    return np.concatenate([
             np.array(A.shape),
             candidates.flatten(),
             voters.flatten(),
             A.flatten()])    
 
-    return Profile.fromNumpy(npProfile)
-
-def generateVCRProfilesByRadius(RNG,
+def generateVCRProfileByRadius(RNG,
     C:int, V:int,
     radiusParams: Dict[int,Tuple[float,float]]) -> Profile:
 
@@ -181,14 +179,96 @@ def generateVCRProfilesByRadius(RNG,
         radiiV = RNG.uniform(low=rMin, high=rMax, size=C)
         candidates = np.dstack((cPositions, radiiC))[0]
         voters = np.dstack((vPositions, radiiV))[0]
-        profile = profileFromVotersAndCandidates(voters, candidates)
+        profile = npProfileFromVotersAndCandidates(voters, candidates)
         resultProfiles[key].append(profile)
     
     return resultProfiles
 
+def mergeDictLists(d1, d2):
+  keys = set(d1).union(d2)
+  no = []
+  return dict((k, d1.get(k, no) + d2.get(k, no)) for k in keys)
+
+def generateVCRProfilesByRadius(RNG, count:int,
+    C:int, V:int,
+    radiusParams: Dict[int,Tuple[float,float]]) -> List[Profile]:
+    resultProfiles = {k:list() for k in radiusParams.keys()}
+
+    for i in range(count):
+        profiles = generateVCRProfileByRadius(RNG=R, C=C, V=V, radiusParams=radiusParams)
+        resultProfiles = mergeDictLists(resultProfiles, profiles)
+
+    return resultProfiles
+
+def runnerVCRProfilesByRadius(C:int, V:int):
+    RNG=default_rng()
+    distribution = 'uniform'
+    count = 10
+    radiusParams={0:(0.7, 0.7), 1:(1.2, 1.2), 2:(0.7,1.2), 3:(0,2.5)}
+
+    path = "resources/random/numpy/vcr-{}-{}R-{}C{}V.npy"
+
+    profilesByR = generateVCRProfilesByRadius(RNG, count, C, V, radiusParams)
+
+    for rParam, profiles in profilesByR.items():
+        saveLoc = path.format(distribution, rParam, C, V)
+        print("Saving to : {}".format(saveLoc))
+        with open(saveLoc, 'wb') as f:
+            np.save(file=f, arr=profiles, allow_pickle=False)
+
+
 #%%
+runnerVCRProfilesByRadius(30,30)
+
+
+#%%
+fig, (ax1, ax2) = plt.subplots(1,2, figsize=(14,6))
 d = generateVCRProfilesByRadius(RNG=R, C=20, V=20, radiusParams={0:(1,1), 1:(0,4)})
-sns.heatmap((getVCRProfileInCRVROrder(d[0][0]).A), cmap=['black', 'gray'])
-plt.show()
-sns.heatmap((getVCRProfileInCRVROrder(d[1][0]).A), cmap=['black', 'gray'])
+sns.heatmap((d[1][0].A), cmap=['black', 'gray'], ax=ax1)
+sns.heatmap((getVCRProfileInCRVROrder(d[1][0]).A), cmap=['black', 'gray'], ax=ax2)
+ax1.set_title("Initial Approval Matrix")
+ax1.set_xlabel("candidates")
+ax1.set_ylabel("voters")
+
+ax2.set_title("VCR Ordered Approval Matrix")
+ax2.set_xlabel("candidates (reindexed)")
+ax2.set_ylabel("voters (reindexed)")
+
+#%%
+d0 = generateVCRProfileByRadius(RNG=R, C=4, V=4, radiusParams={0:(1,1), 1:(0,4)})
+d1 = generateVCRProfileByRadius(RNG=R, C=4, V=4, radiusParams={0:(1,1), 1:(0,4)})
+
+
+#%%
+d = generateVCRProfilesByRadius(RNG=R, count=10, C=20, V=20, radiusParams={0:(1,1), 1:(0,4)})
+
+#%%
+i = 2
+fig, (ax1, ax2) = plt.subplots(1,2, figsize=(14,6))
+sns.heatmap((getVCRProfileInCRVROrder(Profile.fromNumpy(d[0][i])).A), cmap=['black', 'gray'], ax=ax1)
+sns.heatmap((getVCRProfileInCRVROrder(Profile.fromNumpy(d[1][i])).A), cmap=['black', 'gray'], ax=ax2)
+ax1.set_title("Initial Approval Matrix")
+ax1.set_xlabel("candidates")
+ax1.set_ylabel("voters")
+
+ax2.set_title("VCR Ordered Approval Matrix")
+ax2.set_xlabel("candidates (reindexed)")
+ax2.set_ylabel("voters (reindexed)")
+
+#%%
+P44_0 = np.load('resources/random/numpy/vcr-uniform-0R-30C30V.npy')
+P44_1 = np.load('resources/random/numpy/vcr-uniform-1R-30C30V.npy')
+P44_2 = np.load('resources/random/numpy/vcr-uniform-2R-30C30V.npy')
+P44_3 = np.load('resources/random/numpy/vcr-uniform-3R-30C30V.npy')
+
+#%%
+print(Profile.fromNumpy(P44_3[0]))
+
+#%%
+i = 2
+fig, (ax1, ax2) = plt.subplots(2,2, figsize=(14,12))
+sns.heatmap(getVCRProfileInCRVROrder(Profile.fromNumpy(P44_0[i])).A, cmap=['black', 'gray'], ax=ax1[0])
+sns.heatmap(getVCRProfileInCRVROrder(Profile.fromNumpy(P44_1[i])).A, cmap=['black', 'gray'], ax=ax1[1])
+sns.heatmap(getVCRProfileInCRVROrder(Profile.fromNumpy(P44_2[i])).A, cmap=['black', 'gray'], ax=ax2[0])
+sns.heatmap(getVCRProfileInCRVROrder(Profile.fromNumpy(P44_3[i])).A, cmap=['black', 'gray'], ax=ax2[1])
 
